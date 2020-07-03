5aeb2ffd577dbfb0fce4ed53869c8dc139e9e118
Kailash Giri <kpgiri99@gmail.com>
2020-07-03 12:31:09 +0530

    Add files via upload

---
+++ Blink_Led_Demo/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+
+#define LED	13
+
+void setup () {
+	Serial.begin(115200);
+	Serial.println (".");
+	pinMode(LED,OUTPUT);
+}
+
+void loop () {
+	digitalWrite(LED, LOW);
+	Serial.println("Led Off");
+	delay (300);
+	digitalWrite(LED, HIGH);
+	Serial.println("Led On");
+	delay (300);
+}
